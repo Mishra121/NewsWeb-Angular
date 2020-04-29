df3280ae72a9bd5f983c1be14cb42f1cb8e88042
Vibhu <vibhu98mishra@gmail.com>
2020-04-29 16:49:57 +0530

    App component search and filter functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
+import { NewsService } from './shared/news.service';
+import { NewsModel } from './shared/news.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NewsWeb';
+
+  searchText = "";
+  resultSet : any;
+
+  constructor(private newsService : NewsService) { }
+
+  filterNewsByCategory(text: string){
+    console.log(text);
+
+    this.newsService.GetCategory(text).subscribe((result) => {
+      this.newsService.newsArticles = [];
+
+      this.resultSet = result;
+
+      this.resultSet.articles.forEach(element => {
+        this.newsService.newsArticles.push(this.newsService.CustomMapper(element));
+      });
+    })
+  }
+
+  searchNews(text: string){
+   
+    this.newsService.GetSearch(text)
+    .subscribe((result) => {
+      this.newsService.newsArticles = [];
+
+      this.resultSet = result;
+      
+      this.resultSet.articles.forEach(element => {
+        this.newsService.newsArticles.push(this.newsService.CustomMapper(element))
+      })
+    })
+  }
 }
